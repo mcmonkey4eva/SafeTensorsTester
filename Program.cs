0eed046f9029ae82ecb1e551358d689bc5c03dc2
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2023-01-25 09:27:59 -0800

    scan some key data out too

--- Program.cs
+++ Program.cs
@@ -28,8 +28,28 @@ if (len < 8 + headerSize || headerSize <= 0 || headerSize > 100_000_000)
 byte[] headerBytes = new byte[headerSize];
 stream.Read(headerBytes, 0, (int) headerSize);
 string header = Encoding.UTF8.GetString(headerBytes);
-string formatted = JToken.Parse(header).ToString(Formatting.Indented);
-string data = $"Header length: {headerSize}, as string: {header.Length}, full file len {len}:\n\n----------\n{formatted}\n--------";
+JToken token = JToken.Parse(header);
+string formatted = token.ToString(Formatting.Indented);
+string data = $"Header length: {headerSize}, as string: {header.Length}, full file len {len}:\n\n----------\n{formatted}\n--------\n";
+foreach ((string key, JToken subToken) in token.ToObject<Dictionary<string, JToken>>())
+{
+    Dictionary<string, JToken> value = subToken.ToObject<Dictionary<string, JToken>>();
+    if (value.TryGetValue("data_offsets", out JToken offsets))
+    {
+        long min = (long)offsets[0];
+        long max = (long)offsets[1];
+        long dist = max - min;
+        if (dist <= 8)
+        {
+            stream.Position = 8 + headerSize + min;
+            byte[] subData = new byte[dist];
+            stream.Read(subData, 0, (int) dist);
+            string val = dist == 4 ? BitConverter.ToSingle(subData).ToString() : "?";
+            string val2 = dist == 4 ? BitConverter.ToInt32(subData).ToString() : "?";
+            data += $"{key} has len {dist}: {Convert.ToHexString(subData)} = {val} = {val2}\n";
+        }
+    }
+}
 Console.WriteLine(data);
 File.WriteAllText("out_raw.json", data);
 Console.ReadLine();
